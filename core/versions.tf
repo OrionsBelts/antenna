49048fe05d695e85aa3c5f9a780d956c597fdcb8
Michael Perrotte <mike@mikecorp.ca>
2020-09-01 22:35:33 -0400

    feat(core): added terraform versions file

---
+++ core/versions.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "terraform-providers/digitalocean"
+    }
+    local = {
+      source = "hashicorp/local"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+    template = {
+      source = "hashicorp/template"
+    }
+  }
+  required_version = ">= 0.13"
+}
