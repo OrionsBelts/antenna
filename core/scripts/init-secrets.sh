eb57250e2d61a250f6264d96921baf480a5f18be
Michael Perrotte <mike@mikecorp.ca>
2020-09-17 04:38:45 -0400

    feat(core): added script to initialise secrets

---
+++ core/scripts/init-secrets.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+##
+# Description
+#
+#
+##
+
+# Exit immediately if there is an error
+set -e
+
+# Check to see that deps are installed
+jq --version
+yq --version
+
+# Validate Environment Variables
+[[ -z "${GITHUB_WORKSPACE}" ]] && echo "Missing Env Var" && exit 1
+[[ -z "${TF_VAR_do_subdomain}" ]] && echo "Missing Env Var" && exit 1
+[[ -z "${TF_VAR_do_domain}" ]] && echo "Missing Env Var" && exit 1
+
+# Global Variables
+STACK_FILE="${GITHUB_WORKSPACE}/stack.yml"
+DOMAIN="${TF_VAR_do_subdomain}.${TF_VAR_do_domain}"
+GATEWAY_URI="https://${DOMAIN}/"
+
+# Check to see if the stack file is present before doing operations
+if [ ! -f "${STACK_FILE}" ]; then
+  echo "Error: ${STACK_FILE} not found."
+  exit 1
+fi
+
+# Import Common Utils
+source "${GITHUB_WORKSPACE}/core/scripts/common.sh"
+
+# Login to instance
+echo "INFO: login to openfaas"
+openfaas_login "${GITHUB_WORKSPACE}/core/terraform.tfstate" "${GATEWAY_URI}"
+
+# Collect **NEEDED** secret keys
+FN_SECRET_LIST=$(cat "${STACK_FILE}" | yq -r '.functions[].secrets[]?')
+
+# Create secrets in open-faas instance
+for SECRET_KEY in ${FN_SECRET_LIST}; do
+  SECRET_KEY_FORMATTED=$(echo "${SECRET_KEY}" | tr '[:lower:]' '[:upper:]' | sed s/-/_/g)
+
+  SECRET_VALUE=$(echo "${SECRETS}" | jq -r --arg KEY "${SECRET_KEY_FORMATTED}" '.[$KEY]')
+  echo "Adding secret: ${SECRET_KEY}.."
+  openfaas_setSecret "${SECRET_KEY}" "${SECRET_VALUE}" "${GATEWAY_URI}"
+done
