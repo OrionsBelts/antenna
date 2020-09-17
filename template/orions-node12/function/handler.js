afa8b75172436c5da4e2720159b2b65f91824099
Michael Perrotte <mike@mikecorp.ca>
2020-09-17 04:43:14 -0400

    feat(template): updated node12 template

--- template/orions-node12/function/handler.js
+++ template/orions-node12/function/handler.js
@@ -1,11 +1,15 @@
 'use strict'
 
 module.exports = async (event, context) => {
-  const result = {
-    status: `Received input: ${JSON.stringify(event.body)}`
-  }
+  const { path, query, method } = event
+  const results = []
+  const meta = { path, query, method }
 
   return context
     .status(200)
-    .succeed(result)
+    .succeed({
+      status: 'ok',
+      results,
+      meta,
+    })
 }
