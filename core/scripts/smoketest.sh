1f90ad66e754f99beccb4f5b32e80133c8e6af9e
Michael Perrotte <mike@mikecorp.ca>
2020-09-17 03:26:10 -0400

    feat(core): refactored smoketest script
    - Script is now requiring the common script file

--- core/scripts/smoketest.sh
+++ core/scripts/smoketest.sh
@@ -13,6 +13,7 @@ set -e
 [[ -z "${GITHUB_WORKSPACE}" ]] && echo "Missing Env Var" && exit 1
 [[ -z "${TF_VAR_do_subdomain}" ]] && echo "Missing Env Var" && exit 1
 [[ -z "${TF_VAR_do_domain}" ]] && echo "Missing Env Var" && exit 1
+[[ -z "${TF_VAR_do_token}" ]] && echo "Missing Env Var" && exit 1
 [[ -z "${FUNCTION_PREFIX}" ]] && echo "Missing Env Var" && exit 1
 [[ -z "${DO_REGISTRY_AUTH}" ]] && echo "Missing Env Var" && exit 1
 
@@ -29,146 +30,51 @@ FILENAME="${FUNC_NAME}.yml"
 SECRET_KEY="some-secret"
 SECRET_VALUE="super-secret-value"
 
-# Function Declarations
-docker_auth() {
-  mkdir -p "${HOME}/.docker/"
-  local AUTH=$(curl -s \
-    -H "Content-Type: application/json" \
-    -H "Authorization: Bearer ${TF_VAR_do_token}" \
-    "https://api.digitalocean.com/v2/registry/docker-credentials?read_write=true")
-  echo "${AUTH}" > "${HOME}/.docker/config.json"
-}
-
-openfaas_login() {
-  local PASSWORD=$(cat "${GITHUB_WORKSPACE}/core/terraform.tfstate" | jq -r '.resources[] | select(.name == "password") | .instances[].attributes.result')
-  faas-cli \
-    login \
-    --username admin \
-    --password "${PASSWORD}" \
-    --gateway "${GATEWAY_URI}" \
-    --tls-no-verify
-}
-
-openfaas_createFunc() {
-  faas-cli \
-    new \
-    "${FUNC_NAME}" \
-    --lang="${TEMPLATE}" \
-    --gateway="${GATEWAY_URI}" \
-    --prefix="${FUNCTION_PREFIX}"
-}
-
-openfaas_updateFuncYaml() {
-  # Generate temp yaml
-  cat "${FILENAME}" | \
-  yq \
-    --arg KEY "${SECRET_KEY}" \
-    '.functions.smoketest.secrets = [$KEY] | .configuration.copy = ["./common"] | .version = "1.0"' \
-    --yaml-output \
-  > "${FILENAME}.tmp"
-
-  # Overwrite yaml
-  mv "${FILENAME}.tmp" "${FILENAME}"
-}
-
-openfaas_build() {
-  faas-cli \
-    build \
-    --yaml "${FILENAME}" \
-    --tag sha
-}
-
-openfaas_push() {
-  faas-cli \
-    push \
-    --yaml "${FILENAME}" \
-    --tag sha
-}
-
-openfaas_deploy() {
-  faas-cli \
-    deploy \
-    --yaml "${FILENAME}" \
-    --tag sha \
-    --tls-no-verify
-}
-
-openfaas_setSecret() {
-  local key=$1
-  local value=$2
-
-  faas-cli \
-    secret \
-    create \
-    "${key}" \
-    --from-literal="${value}" \
-    --gateway="${GATEWAY_URI}" \
-    --tls-no-verify
-}
-
-openfaas_invokeFunc() {
-  local name=$1
-
-  faas-cli \
-    invoke \
-    "${name}" \
-    --gateway "${GATEWAY_URI}" \
-    --tls-no-verify
-}
-
-assert_equal() {
-  local actual=$1
-  local expected=$2
-  test "${actual}" == "${expected}" || \
-  (echo "FAILED - expected: \"${expected}\", actual: \"${actual}\""; exit 1)
-}
-
-# Script Logic
-
-# Install `faas-cli`
-curl -sSL https://cli.openfaas.com | sudo sh
+# Import Common Utils
+source ./common.sh
 
 # Grant permission to container registry
 echo "INFO: authenticate docker"
-docker_auth
+docker_auth "${TF_VAR_do_token}"
 
 # Repo root
-cd ".."
+# INFO(mperrotte): in order for the build/deploy command to work
+cd "${GITHUB_WORKSPACE}"
 
 # Login to instance
 echo "INFO: login to openfaas"
-openfaas_login
+openfaas_login "${GITHUB_WORKSPACE}/core/terraform.tfstate" "${GATEWAY_URI}"
 
 # Create smoketest function
 echo "INFO: create smoketest function"
-openfaas_createFunc
+openfaas_createFunc "${FUNC_NAME}" "${TEMPLATE}" "${GATEWAY_URI}" "${FUNCTION_PREFIX}"
 
 # Generate new function context yaml
 echo "INFO: update function context yaml"
-openfaas_updateFuncYaml
+openfaas_updateFuncYaml "${FILENAME}" "${SECRET_KEY}"
 
 # Build smoketest function
 echo "INFO: build smoketest function"
-openfaas_build
+openfaas_build "${FILENAME}"
 
 # Push smoketest function image
 echo "INFO: push smoketest function"
-openfaas_push
+openfaas_push "${FILENAME}"
 
 # Set secret used by function
 echo "INFO: create secret for function"
-openfaas_setSecret "${SECRET_KEY}" "${SECRET_VALUE}"
+openfaas_setSecret "${SECRET_KEY}" "${SECRET_VALUE}" "${GATEWAY_URI}"
 
 # Deploy smoketest function
 echo "INFO: deploy smoketest function"
-openfaas_deploy
+openfaas_deploy "${FILENAME}"
 
 echo "INFO: sleep for 5 seconds (allow function to deploy)"
 sleep 5s
 
 # Invoke function
 echo "INFO: invoke smoketest function"
-RESULT=$(openfaas_invokeFunc "${FUNC_NAME}")
+RESULT=$(openfaas_invokeFunc "${FUNC_NAME}" "${GATEWAY_URI}")
 
 # Validate function has access to secret
 echo "INFO: validate function result"
