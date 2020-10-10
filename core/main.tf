b77420ae4d388b41c120fb8bafb6674ccc447f30
Michael Perrotte <mike@mikecorp.ca>
2020-10-09 23:50:37 -0400

    feat(core): remove terraform output

--- core/main.tf
+++ core/main.tf
@@ -149,20 +149,3 @@ resource "random_password" "password" {
   special          = true
   override_special = "_-#"
 }
-
-# - Output Variables
-output "droplet_ip" {
-  value = digitalocean_droplet.faasd.ipv4_address
-}
-
-output "gateway_url" {
-  value = "https://${var.do_subdomain}.${var.do_domain}/"
-}
-
-output "password" {
-  value = random_password.password.result
-}
-
-output "login_cmd" {
-  value = "faas-cli login -g https://${var.do_subdomain}.${var.do_domain}/ -p ${random_password.password.result}"
-}
